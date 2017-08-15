ecccbeb5468de6e5f7a5250d8026ba68acf69ae4
Wesley Coetzee <coetzeewr@gmail.com>
2017-08-15 17:41:59 +0200

    Increased performance ok pinch and pan

--- src/pages/pinchzoompan/pinchzoompan.ts
+++ src/pages/pinchzoompan/pinchzoompan.ts
@@ -21,6 +21,13 @@ export class PinchzoompanPage {
   currentDeltaX = null;
   currentDeltaY = null;
 
+  limitsSet = false;
+  minDeltaX = null;
+  minDeltaY = null;
+  maxDeltaX = null;
+  maxDeltaY = null;
+  maxScale = 8;
+
   public mediaUrl: any;
   public src: string;
   private gesture: Gesture;
@@ -34,6 +41,17 @@ export class PinchzoompanPage {
   setMediaLoaded =() =>{
     setTimeout(()=>this.mediaLoaded = true, 200);
   };
+
+  setLimits() {
+    if (this.limitsSet || this.image.width === 0 || this.image.height ===0)
+      return;
+
+    this.maxDeltaX = this.image.width / 2;
+    this.maxDeltaY = this.image.height / 2;
+    this.minDeltaX = 0 - this.maxDeltaX;
+    this.minDeltaY = 0 - this.maxDeltaY;
+    this.limitsSet = true;
+  }
 
   ionViewDidLoad() {
     this.image = this.element.nativeElement;
@@ -50,6 +68,7 @@ export class PinchzoompanPage {
    Initialize listeners for gestures
    */
   init = () => {
+
     //create gesture obj w/ ref to DOM element
     this.gesture = new Gesture(this.element.nativeElement);
 
@@ -58,18 +77,24 @@ export class PinchzoompanPage {
 
     this.gesture.on('doubletap', (ev) => {
       this.transforms = [];
-      this.adjustScale += 1;
-      if (this.adjustScale >= 4) this.adjustScale = 1;
+      this.adjustScale *= 2;
+      if (this.adjustScale > this.maxScale) this.adjustScale = 1;
       this.transforms.push('scale(' + this.adjustScale + ')');
+      this.transforms.push('translate(' + this.adjustDeltaX + 'px,' + this.adjustDeltaY + 'px)');
       this.container.style.transform = this.transforms.join(' ');
     });
 
 
     this.gesture.on("pinch", (ev) => {
+
+      this.setLimits();
+
       this.transforms = [];
 
       // Adjusting the current pinch/pan event properties using the previous ones set when they finished touching
       this.currentScale = this.adjustScale * ev.scale;
+      if (this.currentScale > this.maxScale)
+        this.currentScale = this.maxScale;
 
       // Concatinating and applying parameters.
       if (this.currentScale < 1) {
@@ -80,20 +105,24 @@ export class PinchzoompanPage {
       this.transforms.push('scale(' + this.currentScale + ')');
       this.transforms.push('translate(' + this.currentDeltaX + 'px,' + this.currentDeltaY + 'px)');
       this.container.style.transform = this.transforms.join(' ');
+
     });
 
     this.gesture.on("pan", (ev) => {
+
+      this.setLimits();
+
       this.transforms = [];
 
       // Adjusting the current pinch/pan event properties using the previous ones set when they finished touching
-      this.currentScale = this.adjustScale * 1;
+      this.currentScale = this.adjustScale;
       this.currentDeltaX = this.adjustDeltaX + (ev.deltaX / this.currentScale);
       this.currentDeltaY = this.adjustDeltaY + (ev.deltaY / this.currentScale);
 
-      if (this.currentDeltaX > (this.image.width / 2)) this.currentDeltaX = this.image.width / 2;
-      if (this.currentDeltaX < 0-(this.image.width / 2)) this.currentDeltaX = 0-this.image.width / 2;
-      if (this.currentDeltaY > (this.image.height / 2)) this.currentDeltaY = this.image.height / 2;
-      if (this.currentDeltaY < 0-(this.image.height / 2)) this.currentDeltaY = 0-this.image.height / 2;
+      if (this.currentDeltaX > this.maxDeltaX) this.currentDeltaX = this.maxDeltaX;
+      if (this.currentDeltaX < this.minDeltaX) this.currentDeltaX = this.minDeltaX;
+      if (this.currentDeltaY > this.maxDeltaY) this.currentDeltaY = this.maxDeltaY;
+      if (this.currentDeltaY < this.minDeltaY) this.currentDeltaY = this.minDeltaY;
 
       // Concatinating and applying parameters.
       if (this.currentScale < 1) {
@@ -104,15 +133,19 @@ export class PinchzoompanPage {
       this.transforms.push('scale(' + this.currentScale + ')');
       this.transforms.push('translate(' + this.currentDeltaX + 'px,' + this.currentDeltaY + 'px)');
       this.container.style.transform = this.transforms.join(' ');
+
     });
 
 
     this.gesture.on("pinchend", (ev) => {
+
       // Saving the final transforms for adjustment next time the user interacts.
-      this.adjustScale = this.currentScale;    
+      this.adjustScale = this.currentScale;
+
     });
 
     this.gesture.on("panend", (ev) => {
+
       // Saving the final transforms for adjustment next time the user interacts.
       this.adjustDeltaX = this.currentDeltaX;
       this.adjustDeltaY = this.currentDeltaY;
